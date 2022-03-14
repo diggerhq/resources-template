6addad7dc958c794645d845eae19a10530a63e81
alexey <alexey@digger.dev>
2022-03-14 12:27:36 +0000

    make if conditions more explicit

--- main/resources.template.tf
+++ main/resources.template.tf
@@ -5,32 +5,33 @@
         module "app_rds_{{resource.name}}" {
           source = "../rds"
           
-          {% if resource.rds_instance_class %}
+          {% if resource.rds_instance_class is defined %}
           instance_class = "{{resource.rds_instance_class}}"
           {% endif %}
 
-          {% if resource.rds_engine %}
+          {% if resource.rds_engine is defined %}
           engine = "{{resource.rds_engine}}"
           {% endif %}
           
-          {% if resource.rds_engine_version %}
+          {% if resource.rds_engine_version is defined %}
           engine_version = "{{resource.rds_engine_version}}"
           {% endif %}
           
-          {% if resource.rds_allocated_storage %}
+          {% if resource.rds_allocated_storage is defined %}
           allocated_storage = "{{resource.rds_allocated_storage}}"
           {% endif %}
 
-          {% if resource.database_snapshot_identifier %}
+          {% if resource.database_snapshot_identifier is defined %}
           snapshot_identifier = "{{resource.rds_snapshot_identifier}}"
           {% endif %}
 
-          {% if resource.database_iops %}
+          {% if resource.database_iops is defined %}
           iops = "{{resource.rds_iops}}"
           {% endif %}
 
           identifier_prefix = "${var.app}-${var.environment}"
           publicly_accessible = false
+          subnet_ids = var.vpc_subnet_ids
         }
 
         output "DGVAR_DATABASE_URL" {
@@ -38,7 +39,13 @@
         }
 
     {% elif resource.resource_type == "redis" %}
-    {% elif reosource.resource_type == "mongodb" %}  
+        module "app_redis_{{resource.name}}" {
+        source = "../redis"
+        }
+    {% elif reosource.resource_type == "mongodb" %}
+        module "app_mongodb_{{resource.name}}" {
+        source = "../mongodb"
+        }
     {% endif %}
 
 {% endfor %}
