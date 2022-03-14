13121c1281ef050f22bc140de5284f49a459be37
alexey <alexey@digger.dev>
2022-03-14 14:28:14 +0000

    remove newlines

--- main/resources.template.tf
+++ main/resources.template.tf
@@ -7,27 +7,21 @@
           {%- if resource.rds_instance_class is defined %}
           instance_class = "{{resource.rds_instance_class}}"
           {% endif %}
-
           {%- if resource.rds_engine is defined %}
           engine = "{{resource.rds_engine}}"
           {%+ endif %}
-          
           {%- if resource.rds_engine_version is defined %}
           engine_version = "{{resource.rds_engine_version}}"
           {% endif %}
-          
           {%- if resource.rds_allocated_storage is defined %}
           allocated_storage = "{{resource.rds_allocated_storage}}"
           {% endif %}
-
           {%- if resource.database_snapshot_identifier is defined %}
           snapshot_identifier = "{{resource.rds_snapshot_identifier}}"
           {% endif %}
-
           {%- if resource.database_iops is defined %}
           iops = "{{resource.rds_iops}}"
           {% endif %}
-
           identifier_prefix = "${var.app}-${var.environment}"
           publicly_accessible = false
           subnet_ids = var.vpc_subnet_ids
